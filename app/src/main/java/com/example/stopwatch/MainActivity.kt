ef6701bd60341483a71449bb9336f078be946004
Qui Van <quivan3396@yahoo.com>
2022-07-25 12:48:21 -0400

    start, stop, and startOrStop methods functionality

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -7,20 +7,40 @@ import com.example.stopwatch.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var isStarted = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnStart.setOnClickListener {
+            startOrStop()
+        }
+
+        binding.btnReset.setOnClickListener {
+            reset()
+        }
     }
 
     private fun start() {
-
+        binding.btnStart.text = getString(R.string.stop)
+        isStarted = true
     }
+
     private fun stop() {
-
+        binding.btnStart.text = getString(R.string.start)
+        isStarted = false
     }
+
     private fun reset() {
 
+    }
+
+    private fun startOrStop() {
+        if (isStarted) {
+            stop()
+        } else
+            start()
     }
 }
